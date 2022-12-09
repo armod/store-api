509586ae9408fb669e067f1033f70a78768efbc9
armod <armods@gmail.com>
2022-12-09 05:02:53 +0100

    start setup

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-console.log('04 Store API')
+require('dotenv').config()
+// asyn error
+
+const express = require('express')
+const app = express()
+
+const notFoundMiddleware = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
+
+// middleware
+app.use(express.json())
+
+// rootes
+app.get('/', (req, res) => {
+  res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>')
+})
+
+// products route
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
+
+const port = process.env.PORT || 3000
+
+const start = async () => {
+  try {
+    // connsctDB
+    app.listen(port, console.log(`Server listening port ${port}...`))
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
