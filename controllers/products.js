206415b7603a01b11e0fd91c89dfb879ce9b1823
armod <armods@gmail.com>
2022-12-12 09:50:18 +0100

    find

--- controllers/products.js
+++ controllers/products.js
@@ -1,6 +1,9 @@
+const Product = require('../models/product')
+
 const getAllProductsStatic = async (req, res) => {
-  throw new Error('testing async errors')
-  res.status(200).json({ msg: 'products testing route' })
+  // throw new Error('testing async errors')
+  const products = await Product.find({ name: 'vase table' })
+  res.status(200).json({ products, nbHits: products.length })
 }
 const getAllProducts = async (req, res) => {
   res.status(200).json({ msg: 'products route' })
