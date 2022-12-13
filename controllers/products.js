da8970414411c5b6f2e66b0f443f1655efc1d767
armod <armods@gmail.com>
2022-12-13 06:28:06 +0100

    limit and skpip

--- controllers/products.js
+++ controllers/products.js
@@ -42,6 +42,14 @@ const getAllProducts = async (req, res) => {
     const fieldsList = fields.split(',').join(' ')
     result = result.select(fieldsList)
   }
+  // limit and skip
+  const page = Number(req.query.page) || 1
+  const limit = Number(req.query.limit) || 10
+  const skip = (page - 1) * limit
+
+  result = result.skip(skip).limit(limit)
+  // 23
+  // 23 rozbijamy na 4 strony 7 7 7 i 2 na stronie
 
   const products = await result
   res.status(200).json({ products, nbHits: products.length })
